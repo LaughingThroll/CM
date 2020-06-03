dc5538547681c81221214648e57a6405d9bffdd4
LaughingThroll <LaughingThroll@gmail.com>
2020-06-04 00:35:49 +0300

    added scroll-effect on slider and mobile scroll-content

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -7,12 +7,14 @@ import {
   mediaWidthMore815,
   mediaWidthMore600 
 } from './modules/breakpoints'
+// import onWheel from './modules/onWheel'
 import { isNull, limitationsSymbols } from './utils/utils'
+const speedSlide = 700
 
 const currentModel = document.querySelector('.current-model__slider')
 const homePage = document.querySelector('#home')
 
-
+const modelPage = document.querySelector('.model-page')
 const modelPage0000 = document.querySelector('.model-page-0000')
 
 const jsAboutDescr = document.querySelectorAll('.js-about-descr')
@@ -20,12 +22,13 @@ const jsAboutDescr = document.querySelectorAll('.js-about-descr')
 // fourth-design
 const jsPortfolioModel0003Gallery = document.querySelector('#js-portfolio-model-0003__gallery')
 
-
+let currentModelSwiper
 const configSwiper = {
   navigation: {
     nextEl: '.current-model__arrow-next',
     prevEl: '.current-model__arrow-prev',
   },
+  speed: speedSlide,
   pagination: {
     el: '.current-model__pagination',
     bulletClass: 'current-model__pagination-dot',
@@ -37,9 +40,16 @@ const configSwiper = {
   },
   direction: 'vertical', 
   spaceBetween: 300,
+  keyboard: true,
+  // allowTouchMove: false
+  allowTouchMove: false
 }
 
-isNull(currentModel, () => new Swiper(currentModel, configSwiper))
+isNull(currentModel, () => {
+  currentModelSwiper = new Swiper(currentModel, configSwiper)
+})
+
+
 
 jsAboutDescr.forEach(content => isNull(content, () => limitationsSymbols(content, 400)))
 
@@ -63,12 +73,56 @@ isNull(jsPortfolioModel0003Gallery, () => {
   window.addEventListener('resize', mediaWidthLess600)
   window.addEventListener('resize', mediaWidthMore600)
 })
+
+
+// ========================================= WHEEL ==================================
+
+isNull(modelPage, () => {
+  window.addEventListener('wheel', onWheel)
+})
+
+function onWheel(e) {
+
+  if (e.deltaY >= 0) {
+      currentModelSwiper.slideNext(speedSlide, false)
+  } else {
+    currentModelSwiper.slidePrev(speedSlide, false)
+  }
+
+  removeEventListener('wheel', onWheel)
+  setTimeout(() => {
+    window.addEventListener('wheel', onWheel)
+  }, 1000)
+}
+// TODO modalHelper on space
+window.addEventListener('keydown', (e) => {
+  if (e.code === 'Space' || e.repeat) {
+    removeEventListener('wheel', onWheel)
+  }
+})
+
+window.addEventListener('keyup', (e) => {
+  if (e.code === 'Space') {
+    alert('DOING MODAL HELPER')
+    window.addEventListener('wheel', onWheel)
+  }
+})
 // FUTURE work with touchscreen
+let timerTouch
 window.addEventListener('touchstart', function(e) {
-  
+  timerTouch = setTimeout(() => {
+    // currentModelSwiper.destroy()
+   
+    // currentModelSwiper.destroy()
+    // currentModelSwiper = new Swiper(currentModel, configSwiper)
+    // currentModelSwiper.init()
+    // console.log(configSwiper)
+  }, 800)
   
 }) 
-
-window.addEventListener('touchmove', function(e) {
-  // console.log(e)
+window.addEventListener('touchmove', () => {
+  clearTimeout(timerTouch)
+})
+window.addEventListener('touchend', () => {
+  clearTimeout(timerTouch)
 })
